d8f57a091cc09ddbb4c6da05e145952809efb3a8
memeLordo <simaforest@BAD-BOY>
2023-11-28 01:39:26 +0300

    ID_chat operating

--- main.py
+++ main.py
@@ -1,44 +1,44 @@
-import json
+# import json
+from telethon import functions
 from telethon.sync import TelegramClient
-from telethon import functions, types
-from telethon.sessions import StringSession
+
 import config
 
 # Use your own values from my.telegram.org
 
-
 # # The first parameter is the .session file name (absolute paths allowed)
 # with TelegramClient('anon', api_id, api_hash) as client:
-#     client.loop.run_until_complete(client.send_message('me', 'Hello, myself!'))
-client = TelegramClient('lol', config.api_id2, config.api_hash2)
-SEARCHED_DIRS = ['Новые FA','Free assist']
+#     client.loop.run_until_complete(client.send_message('me', 'Hello!'))
+client = TelegramClient("lol", config.api_id2, config.api_hash2)
+SEARCHED_DIRS = ["Новые FA", "Free assist"]
+
 
 async def main():
+    async for dialog in client.iter_dialogs():
+        if dialog.name == "Golubin | Assistant":
+            print(dialog.id)
+
     request = await client(functions.messages.GetDialogFiltersRequest())
     for dialog_filter in request:
         result = dialog_filter.to_dict()
         try:
-
-            if(result['title'] in SEARCHED_DIRS):
-                print(result['title'])
-                
-                for channel in result['pinned_peers']:
+            if result["title"] in SEARCHED_DIRS:
+                print(result["title"])
+                for channel in result["pinned_peers"]:
                     try:
-                        print(channel['channel_id'])
+                        print(channel["channel_id"])
                     except KeyError:
                         pass
 
-                for channel in result['include_peers']:
+                for channel in result["include_peers"]:
                     try:
-                        print(channel['channel_id'])
+                        print(channel["channel_id"])
                     except KeyError:
                         pass
         except KeyError:
-           pass 
-        # if(dialog_filter.to_dict()[0]=='Новые FA'):
-        #     print('ok!')
-        # print(dialog_filter.to_dict())
-        # print(json.dumps(result))
+            print(dialog_filter.to_dict())
+    # print(json.dumps(result))
+
 
 with client:
     client.session.save_entities = False
