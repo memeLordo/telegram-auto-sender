9303546ef5e4489de45b529b68a7f8fb0d932dec
memeLordo <simaforest@BAD-BOY>
2023-11-27 23:42:18 +0300

    List Walking in ID's

--- config.py
+++ config.py
@@ -1,3 +1,7 @@
+#my id
+my_api_id = 25065727
+my_api_hash = '3a71d090e43792526725d63bef945ce3'
+
 #elvir 2nd
 api_id2 = 29986266
-api_hash2 = 3691961865c8cb2563797593c33ca27a
+api_hash2 = '3691961865c8cb2563797593c33ca27a'
