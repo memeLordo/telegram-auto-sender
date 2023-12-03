c36d850f2d3865ac15584567d1e40b265d51400a
memeLordo <simaforest@BAD-BOY>
2023-12-03 18:55:16 +0300

    reply working restucurise

--- reply_handler.py
+++ reply_handler.py
@@ -1,6 +1,6 @@
 import asyncio
 
-from telethon import TelegramClient, events, functions
+from telethon import TelegramClient, events
 from telethon.types import User
 
 import config
@@ -13,7 +13,8 @@ def say_hi(name):
         message = f'Привет {name}, рад, что вы откликнулись на вакансию 🔥\n'
     message = (
         message
-        + 'Расскажите подробнее о вашем опыте работы, чтобы я смог узнать о вас побольше)'
+        + 'Расскажите подробнее о вашем опыте работы,' +
+        ' чтобы я смог узнать о вас побольше)'
     )
     return message
 
@@ -26,97 +27,149 @@ reply_massage = (
     '1FAIpQLScQ4MXsn-Qrl38tRwgB6O5LPXrGt2Wasv8H5hCvA-N5H4w2Hw/viewform\n'
     'После этого отпишитесь, например "+", и мы продолжим диалог.'
 )
-client1 = TelegramClient('session_reply1', config.api_id, config.api_hash)
-client2 = TelegramClient('session_reply2', config.api_id2, config.api_hash2)
-client3 = TelegramClient('session_reply3', config.api_id2, config.api_hash2)
 
-clients = [client1, client2, client3
+reply_to_form = (
+    'Отлично. Теперь ваша заявка рассмотрена и отправлена на утверждение '
+    'директору. Если вы нам подойдёте, то я свяжусь с вами. '
+    'И если у вас остались вопросы, то смело задавайте их!'
+)
 
-           ]
+
+client1 = TelegramClient('session1', config.api_id, config.api_hash)
+client2 = TelegramClient('session2', config.api_id2, config.api_hash2)
+client3 = TelegramClient('session3', config.api_id2, config.api_hash2)
 my_client = TelegramClient('anon', config.my_api_id, config.my_api_hash)
+
+clients = [client1, client2, client3]
+
+####################################################
+
+
+async def sent_reply_start(client, bebra):
+
+    first_name = bebra.first_name.split(' ')[0]
+    print(first_name)
+    await client.send_read_acknowledge(bebra.id)
+    async with client.action(bebra, 'typing'):
+        await asyncio.sleep(4)
+        await client.send_message(bebra, say_hi(bebra.first_name))
+        print(f'Новое сообщение от: {bebra.first_name}')
+
+    async with client.action(bebra, 'typing'):
+        await asyncio.sleep(5)
+        await client.send_message(bebra, reply_massage)
+        print('reply message sent')
+
+
+async def sent_reply_to_form(client, bebra):
+    await client.send_read_acknowledge(bebra.id)
+    async with client.action(bebra, 'typing'):
+        await asyncio.sleep(5)
+        await client.send_message(bebra, reply_to_form)
+        print('form message sent')
+
+
+####################################################
+
+
+async def match_sent_message(client, user, message):
+    match message:
+        case 'работа' | 'ассистент':
+            await client.loop.create_task(
+                sent_reply_start(client, user)
+            )
+        case '+':
+            await client.loop.create_task(
+                sent_reply_to_form(client, user)
+            )
+
+
+####################################################
 
 
 @client1.on(events.NewMessage)
-async def handle_new_message(event):
+async def handle_new_message1(event):
     try:
-        if 'работа' == event.raw_text.lower():
-            # print(event)
-            bebra = await event.get_sender()
-            await client1.loop.create_task(sent_reply(client1, bebra))
+        bebra = await event.get_sender()
+        await match_sent_message(client1,
+                                 bebra,
+                                 event.raw_text.lower())
     except Exception as e:
         print(repr(e))
-        # bebra = await my_client.get_entity(event.original_update.user_id)
-        # print(bebra.first_name)
 
-# replied = await event.get_reply_message()
-# sender = replied.sender
-# print(sender.username)
-# # await event.answer(f'Hi, {sender.username}')
-# await event.reply(f'hi, {sender}!')
+
+@client2.on(events.NewMessage)
+async def handle_new_message2(event):
+    try:
+        bebra = await event.get_sender()
+        await match_sent_message(client2,
+                                 bebra,
+                                 event.raw_text.lower())
+    except Exception as e:
+        print(repr(e))
+
+
+@client3.on(events.NewMessage)
+async def handle_new_message3(event):
+    try:
+        bebra = await event.get_sender()
+        await match_sent_message(client3,
+                                 bebra,
+                                 event.raw_text.lower())
+
+    except Exception as e:
+        print(repr(e))
+
+
+####################################################
 
 
 async def check_new_messages():
 
-    await client1.start()
-    # request = await clients[0](functions.messages.GetDialogFiltersRequest())
-    dialogs = client1.iter_dialogs()
-
-    # ch = await client.get_entity(1515430527)
-    # title = client(functions.channels.GetFullChannelRequest(channel=ch))
-    # print(ch.title)
-
-    # channels = await client.get_dialogs(folder=2)
-    # for channel in channels:
-    #     print(channel)
-    #     print('\n\n')
+    try:
+        await client.start()
+        dialogs = client.iter_dialogs()
+    except Exception as e:
+        print(repr(e))
 
     async for dialog in dialogs:
-        # print(dialog.entity)
         try:
             if not isinstance(dialog.entity, User):
                 continue
-            if 'работа' == str(dialog.message.message).lower():
-                bebra = dialog.entity
-                await client1.loop.create_task(
-                    sent_reply(client1, bebra)
-                )
+            bebra = dialog.entity
+            await match_sent_message(client,
+                                     bebra,
+                                     str(dialog.message.message).lower())
 
-                # print(dialog.name)
+            # print(dialog.name)
         except Exception as e:
             print(repr(e))
 
 
-async def sent_reply(client, bebra):
+####################################################
 
-    print(bebra.first_name.split(' ')[0])
-    await client.send_read_acknowledge(bebra.id)
-    async with client.action(bebra, 'typing'):
-        await asyncio.sleep(3)
 
-        match bebra.first_name:
-            case None:
-                print('Новое сообщение is None')
-                await client.send_message(bebra, say_hi(bebra.first_name))
-            case _:
-                await client.send_message(bebra, say_hi(bebra.first_name))
-                print(f'Новое сообщение от: {bebra.first_name}')
-        async with client.action(bebra, 'typing'):
-            await asyncio.sleep(3)
-            await client.send_message(bebra, reply_massage)
-        print('reply message sent')
+def start_event_handler():
+    try:
+        loop = asyncio.get_event_loop()
+        client1.start()
+        client2.start()
+        client3.start()
+        loop.run_forever()
+    except Exception as e:
+        print(repr(e))
+
+
+####################################################
 
 
 if __name__ == '__main__':
-    # for client in clients[0]:
-    # client1.start()
-    try:
-        client1.loop.run_until_complete(check_new_messages())
-    except Exception as e:
-        print(repr(e))
-    client1.run_until_disconnected()
-    # client.start()
 
-    # with clients[0] as client:
-    #     client.loop.run_until_complete(main())
-    # with clients[0] as client:
-    #     client.loop.run_until_complete(main())
+    for current_client in clients:
+
+        with current_client as client:
+            client.session.save_entities = False
+            client.loop.run_until_complete(check_new_messages())
+
+    print('done')
+    start_event_handler()
