4ec84c87657630249632b122ca05831aec8266c0
memeLordo <serafim.kripachev@gmail.com>
2023-12-06 23:01:39 +0300

    cleanup code
    Removed comments, empty spaces and unused vars.
    Resigned logging.

--- date_by_count.py
+++ date_by_count.py
@@ -10,7 +10,7 @@ import config
 # from messages_config import ad_1, ad_2, ad_kazan
 # from telethon.tl import types
 logger.remove()
-logger.add(sys.stderr, level="CRITICAL")
+logger.add(sys.stderr, level="SUCCESS")
 
 client1 = TelegramClient('count1', config.api_id, config.api_hash)
 client2 = TelegramClient('count2', config.api_id2, config.api_hash2)
@@ -33,47 +33,27 @@ async def start():
     await count_date(request)
     if client != clients[-1]:
         logger.debug(f'Current count: {count}')
+    print()
 
 
 async def count_date(request):
-    # global me, me_id
-    # me = await client.get_me()
-    # me_id = int(me.id)
-    print(f'Counting in {show_client(client)}')
+    logger.warning(f'Counting in {show_client(client)}')
     _data = []
     for dialog_filter in request:
         result = dialog_filter.to_dict()
         for key, value in result.items():
-            # logger.info(f'{key} {value}')
 
             if key == 'title':
                 if value == 'Входящие':
-                    # logger.warning(result)
-                    # logger.info(me)
-                    # print(result)
-                    _data = _data + result['exclude_peers']
+                    _data += result['exclude_peers']
                 else:
-                    _data = _data + result['include_peers']
+                    _data += result['include_peers']
 
                 _data = _data + result['pinned_peers']
 
     await count_users_in_dir(_data)
 
-    # case 'Free assist':
-    #     await send_message_to_channel(result, ad_1)
-    #     await asyncio.sleep(3)
-    #
-    # case 'Новые FA':
-    #     await send_message_to_channel(result, ad_2)
-    #     await asyncio.sleep(3)
-    #
-    # case 'КазаньSMS':
-    # await send_message_to_channel(result, ad_kazan)
-    #     await asyncio.sleep(3)
-    #
-    # logger.success('Sent!')
-
-    print(count)
+    logger.success(count)
 
 
 async def exclude_users(data):
@@ -91,24 +71,17 @@ async def exclude_users(data):
         _user = await client.get_entity(_peer_user)
         logger.debug(f'{_user.first_name} {_user.last_name}')
         arr.append(_user)
-        # print(arr)
-    logger.success('Filling excluding data done!')
+        # logger.success(arr)
+    logger.info('Filling excluding data done!')
     return arr
 
 
 async def count_users_in_dir(_data):
 
     exclude_user_list = await exclude_users(_data)
-    logger.warning(len(exclude_user_list))
-    # TODO: useful hint - accounts without username should be passed
+    logger.debug(len(exclude_user_list))
     dialogs = await client.get_dialogs()
     data = [x.entity for x in dialogs if x.entity not in exclude_user_list]
-
-    # for _user in data:
-    #     if not isinstance(_user, types.User) or _user.bot:
-    #         continue
-    #     logger.opt(colors=True).debug(
-    #         f'<magenta>{_user.first_name} {_user.last_name}</magenta>')
 
     for bebra in data:
 
@@ -121,16 +94,11 @@ async def count_users_in_dir(_data):
             limit=1,
             from_user=bebra
         )
-        # my_messages = await client.get_messages(
-        #     entity=my_user.username,
-        #     reverse=True,
-        #     limit=1,
-        #     from_user =
-        # )
+
         for message in client_messages:
             logger.debug(f'{bebra.first_name} {bebra.last_name}')
             for date in todate:
-                # print(date, todate)
+                # logger.success(date, todate)
                 input_date = datetime.strptime(date, '%d.%m.%y').date()
                 message_date = message.date.date()
                 # logger.debug(f'{input_date} - {message_date}')
@@ -142,26 +110,7 @@ async def count_users_in_dir(_data):
                     # date = date.strftime('%d.%m.%y')
 
                     count[date] = count[date]+1
-
                     logger.debug(count)
-        # if not client_messages:
-    #     logger.info('None')
-    #     continue
-    # logger.debug(client_messages[0])
-
-    # print(client_messages[0].date)
-
-    # for message in client_messages:
-    #     logger.warning(message)
-    #     # logger.info(me)
-    #     # peer_user = await client.get_entity(message.peer_id)
-    #     # peer_dict = peer_user.to_dict()
-    #     logger.debug(message.date)
-
-    # # logger.info(message.date)
-    # break
-
-    # logger.debug(my_user)
 
 
 def choose_date():
@@ -181,5 +130,6 @@ if __name__ == '__main__':
         with current_client as client:
             client.session.save_entities = False
             client.loop.run_until_complete(start())
+
     for key, value in count.items():
-        print(key + ' : ' + value)
+        print(f'{key} {value}')
