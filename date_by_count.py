1b31e0849637a2034fd1008172eaf5285b424018
memeLordo <serafim.kripachev@gmail.com>
2023-12-11 01:57:18 +0300

    still working file

--- date_by_count.py
+++ date_by_count.py
@@ -29,16 +29,17 @@ def show_client(client):
 
 @logger.catch
 async def start():
+    logger.warning(f'Counting in {show_client(client)}')
     # request = await client(functions.messages.GetDialogFiltersRequest())
     # await count_date()
-
+    await count_users()
     if client != clients[-1]:
         logger.debug(f'Current count: {count}')
     # print()
 
 
 # async def count_date(request):
-#     logger.warning(f'Counting in {show_client(client)}')
+
 #     _data = []
 #     for dialog_filter in request:
 #         result = dialog_filter.to_dict()
@@ -63,9 +64,28 @@ async def exclude_users():
     return users
 
 
-async def count_users_in_dir():
+async def count_users():
     users = await exclude_users()
-#
+    messages_data = []
+    for bebra in users:
+        client_messages = await client.get_messages(
+            entity=bebra,
+            reverse=True,
+            limit=1,
+            from_user=bebra
+        )
+        if client_messages and client_messages.total < 30:
+            messages_data.append(client_messages)
+            logger.trace(client_messages.total)
+    # messages_data = filter(lambda x: x.total < 30, messages_data)
+    for date in todate:
+        input_date = datetime.strptime(date, '%d.%m.%y').date()
+        count[date] += len(
+            list(filter(lambda x: x[0].date.date()
+                 == input_date, messages_data))
+        )
+
+
 #     exclude_user_list = await exclude_users(_data)
 #     logger.debug(len(exclude_user_list))
 #     dialogs = await client.get_dialogs()
@@ -76,12 +96,7 @@ async def count_users_in_dir():
 #         if not isinstance(bebra, types.User) or bebra.bot:
 #             continue
 #
-#         client_messages = await client.get_messages(
-#             entity=bebra,
-#             reverse=True,
-#             limit=1,
-#             from_user=bebra
-#         )
+#
 #
 #         for message in client_messages:
 #             logger.debug(f'{bebra.first_name} {bebra.last_name}')
