7ce9ed3ff17803ed9d5269167cb1bb6b56d38a4c
memeLordo <serafim.kripachev@gmail.com>
2023-12-11 14:44:04 +0300

    cleanup working code with threads
    Still takes 6 mins to run the first client, insteard of 5 in the old programm.

--- date_by_count.py
+++ date_by_count.py
@@ -2,13 +2,11 @@ import sys
 from datetime import datetime
 
 from loguru import logger
-from telethon import functions, types
+from telethon import types
 from telethon.sync import TelegramClient
 
 import config
 
-# from messages_config import ad_1, ad_2, ad_kazan
-# from telethon.tl import types
 logger.remove()
 logger.add(sys.stderr, level="DEBUG")
 
@@ -30,90 +28,43 @@ def show_client(client):
 @logger.catch
 async def start():
     logger.warning(f'Counting in {show_client(client)}')
-    # request = await client(functions.messages.GetDialogFiltersRequest())
-    # await count_date()
+
     await count_users()
     if client != clients[-1]:
         logger.debug(f'Current count: {count}')
-    # print()
-
-
-# async def count_date(request):
-
-#     _data = []
-#     for dialog_filter in request:
-#         result = dialog_filter.to_dict()
-#         for key, value in result.items():
-#
-#             if key == 'title':
-#                 if value == 'Входящие':
-#                     _data += result['exclude_peers']
-#                 else:
-#                     _data += result['include_peers']
-#
-#                 _data = _data + result['pinned_peers']
-#
-#     await count_users_in_dir(_data)
-#
-#     logger.success(count)
 
 
 async def exclude_users():
     dialogs = await client.get_dialogs(archived=False)
-    users = filter(lambda x: isinstance(x.entity, types.User), dialogs)
+    users = list(filter(lambda x: isinstance(x.entity, types.User), dialogs))
+    logger.info(f'Exclude users succses! {len(users)}')
     return users
+
+
+async def bebra_wrapper(user, data):
+    client_messages = await client.get_messages(
+        entity=user,
+        reverse=True,
+        limit=1,
+        from_user=user
+    )
+    if client_messages and client_messages.total < 30:
+        data.append(client_messages)
+        logger.trace(client_messages.total)
 
 
 async def count_users():
     users = await exclude_users()
     messages_data = []
     for bebra in users:
-        client_messages = await client.get_messages(
-            entity=bebra,
-            reverse=True,
-            limit=1,
-            from_user=bebra
-        )
-        if client_messages and client_messages.total < 30:
-            messages_data.append(client_messages)
-            logger.trace(client_messages.total)
-    # messages_data = filter(lambda x: x.total < 30, messages_data)
+        await client.loop.create_task(bebra_wrapper(bebra, messages_data))
+
     for date in todate:
         input_date = datetime.strptime(date, '%d.%m.%y').date()
         count[date] += len(
             list(filter(lambda x: x[0].date.date()
                  == input_date, messages_data))
         )
-
-
-#     exclude_user_list = await exclude_users(_data)
-#     logger.debug(len(exclude_user_list))
-#     dialogs = await client.get_dialogs()
-#     data = [x.entity for x in dialogs if x.entity not in exclude_user_list]
-#
-#     for bebra in data:
-#
-#         if not isinstance(bebra, types.User) or bebra.bot:
-#             continue
-#
-#
-#
-#         for message in client_messages:
-#             logger.debug(f'{bebra.first_name} {bebra.last_name}')
-#             for date in todate:
-#                 # logger.success(date, todate)
-#                 input_date = datetime.strptime(date, '%d.%m.%y').date()
-#                 message_date = message.date.date()
-#                 # logger.debug(f'{input_date} - {message_date}')
-#                 if input_date == message_date:
-#
-#                     global count
-#
-#                     logger.debug(f'{input_date} - {message_date}')
-#                     # date = date.strftime('%d.%m.%y')
-#
-#                     count[date] = count[date]+1
-#                     logger.debug(count)
 
 
 def choose_date():
