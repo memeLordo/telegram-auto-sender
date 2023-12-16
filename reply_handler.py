b0190d5a83b612b5280d44dd54fa3e23014ac024
memeLordo <serafim.kripachev@gmail.com>
2023-12-16 03:15:28 +0300

    separate reply_handler to two files

--- reply_handler.py
+++ reply_handler.py
@@ -3,44 +3,19 @@ import string
 import sys
 from enum import auto, Enum
 
-import config
 from loguru import logger
 from messages_config import Reply
-from telethon import events, TelegramClient
+from reply_walker import client1, client2, client3, run_message_checker
+from telethon import events
 
 # from telethon.types import User
 
 # logger.remove()
-logger.add(
-    "process.log",
-    format="{time:DD-MM-YYYY at HH:mm:ss} | {level} | {message}",
-    level="INFO",
-    rotation="10 MB",
-    retention="2 days",
-    compression="zip",
-)
+
 logger.add(sys.stderr, level="TRACE")
-####################################################
 
-####################################################
-
-client1 = TelegramClient("r_session1", config.api_id, config.api_hash)
-client2 = TelegramClient("r_session2", config.api_id2, config.api_hash2)
-client3 = TelegramClient("r_session3", config.api_id2, config.api_hash2)
-my_client = TelegramClient("anon", config.my_api_id, config.my_api_hash)
-
-clients = [client1, client2, client3]
 
 ####################################################
-
-
-def show_client(client):
-    if client == client1:
-        return "client1"
-    elif client == client2:
-        return "client2"
-    else:
-        return "client3"
 
 
 def add_count(client):
@@ -50,93 +25,6 @@ def add_count(client):
         pass
     if client == client3:
         pass
-
-
-####################################################
-
-
-@logger.catch
-async def sent_reply_start(client, bebra, error_exit=False):
-    log_name = bebra.first_name
-    first_name = bebra.first_name.split(" ")[0]
-    sender = bebra.username
-    if not error_exit:
-        logger.info(f"{show_client(client)}: got message from {log_name}")
-
-    await asyncio.sleep(1)
-
-    #############
-    try:
-        await client.send_read_acknowledge(sender)
-        async with client.action(bebra, "typing"):
-            await asyncio.sleep(4)
-            await client.send_message(bebra, Reply.say_hi(first_name))
-
-        async with client.action(bebra, "typing"):
-            await asyncio.sleep(5)
-            await client.send_message(sender, Reply.FORM)
-            logger.debug(f"{show_client(client)}: message sent to {log_name}")
-
-    except ValueError:
-        logger.error(f"{show_client(client)}: {log_name} is unknown")
-
-        if error_exit:
-            raise ValueError(f"{show_client(client)}: Still unresolved")
-
-        dialogs = client.iter_dialogs()
-        async for dialog in dialogs:
-            try:
-                if dialog.entity.id == bebra.id:
-                    logger.info(f"{log_name}'s ID found")
-                    bebra = dialog.entity
-                    # logger.debug(bebra)
-                    await sent_reply_start(client, bebra, True)
-                    break
-            except Exception as e:
-                logger.critical(repr(e))
-
-    # except errors.rpcerrorlist.RpcCallFailError:
-    #     logger.error('Telegram is Down. Reloading...')
-    #     client.disconnect()
-    #     await client.start()
-    #     async with client:
-    #         await client.loop.run_until_complete(check_new_messages())
-    #############
-
-
-@logger.catch
-async def sent_reply(client, bebra, message, error_exit=False):
-    log_name = bebra.first_name
-    sender = bebra.username
-    logger.info(f"{show_client(client)}: got message from {log_name}")
-    await asyncio.sleep(1)
-
-    #############
-    try:
-        await client.send_read_acknowledge(sender)
-
-        async with client.action(bebra.username, "typing"):
-            await asyncio.sleep(4)
-            await client.send_message(sender, message)
-            logger.debug(f"{show_client(client)}: message sent to {log_name}")
-    except ValueError:
-        logger.error(f"{show_client(client)}: {log_name} is unknown")
-
-        if error_exit:
-            raise ValueError(f"{show_client(client)}: Still unresolved")
-
-        dialogs = client.iter_dialogs()
-        async for dialog in dialogs:
-            try:
-                if dialog.entity.id == bebra.id:
-                    logger.info(f"{log_name}'s ID found")
-                    bebra = dialog.entity
-                    # logger.debug(bebra)
-                    await sent_reply(client, bebra, message, True)
-                    break
-            except Exception as e:
-                logger.critical(repr(e))
-    #############
 
 
 ####################################################
@@ -241,39 +129,7 @@ async def run_handler(event):
 async def handler(event):
     # sender = await event.get_sender()
     # print(event)
-
     await run_handler(event)
-    #
-    #     case UserState.WAIT_FORM:
-    #         name = event.text  # Save the name wherever you want
-    #         await event.respond(Reply.FORM)
-    #         state_database[who] = UserState.WAIT_AGE
-    #
-    #     case UserState.WAIT_AGE:
-    #         age = event.text  # Save the age wherever you want
-    #         await event.respond(Reply.FINISH)
-    #         # Conversation is done so we can forget the state of this user
-    #         del state_database[who]
-
-
-####################################################
-
-
-@logger.catch
-async def check_new_messages():
-    await client.start()
-    dialogs = client.iter_dialogs()
-    print(dialogs)
-
-    # async for dialog in dialogs:
-    #     try:
-    #         bebra = dialog.entity
-    #         message = str(dialog.message.message).lower()
-    #         await match_sent_message(client, bebra, message)
-    #     except ValueError as e:
-    #         logger.critical(e.__class__.__name__)
-    #         # print(dialog.name)
-    # logger.debug(show_client(client))
 
 
 ####################################################
@@ -289,20 +145,7 @@ def start_event_handler():
     loop.run_forever()
 
 
-@logger.catch
-def run_message_checker():
-    logger.info("Begin check")
-    for current_client in clients:
-        global client
-        with current_client as client:
-            client.session.save_entities = False
-            client.loop.run_until_complete(check_new_messages())
-    logger.success("")
-
-
 ####################################################
-
-
 if __name__ == "__main__":
     run_message_checker()
     start_event_handler()
