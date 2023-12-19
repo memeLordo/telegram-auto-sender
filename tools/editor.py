153369877953a16aad8a7e017ecf1d1f494b0987
memeLordo <serafim.kripachev@gmail.com>
2023-12-20 01:13:44 +0300

    reform editor with replace and emoji

--- tools/editor.py
+++ tools/editor.py
@@ -1,8 +1,43 @@
+import re
 import string
 
 
+def remove_emoji(string):
+    emoji_pattern = re.compile(
+        "["
+        "\U0001F600-\U0001F64F"  # emoticons
+        "\U0001F300-\U0001F5FF"  # symbols & pictographs
+        "\U0001F680-\U0001F6FF"  # transport & map symbols
+        "\U0001F1E0-\U0001F1FF"  # flags (iOS)
+        "\U00002500-\U00002BEF"  # chinese char
+        "\U00002702-\U000027B0"
+        "\U00002702-\U000027B0"
+        "\U000024C2-\U0001F251"
+        "\U0001f926-\U0001f937"
+        "\U00010000-\U0010ffff"
+        "\u2640-\u2642"
+        "\u2600-\u2B55"
+        "\u200d"
+        "\u23cf"
+        "\u23e9"
+        "\u231a"
+        "\ufe0f"  # dingbats
+        "\u3030"
+        "]+",
+        flags=re.UNICODE,
+    )
+    return emoji_pattern.sub(r"", string)
+
+
 def remove_punct(text):
-    result = text.translate(
-        str.maketrans("", "", (string.punctuation).replace("+", ""))
+    def ignore_chars(text, chars_to_ignose=["+", "@"]):
+        for char in chars_to_ignose:
+            text.replace(char, "")
+        return text
+
+    result = remove_emoji(
+        text.translate(
+            str.maketrans("", "", ignore_chars(string.punctuation.join("«»")))
+        )
     )
     return result
