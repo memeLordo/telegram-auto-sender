4e92dd0b7163afc56a02d46c13ae6475cbc668f8
memeLordo <serafim.kripachev@gmail.com>
2023-12-20 01:14:38 +0300

    tools init change

--- tools/__init__.py
+++ tools/__init__.py
@@ -1,12 +1,14 @@
-from tools import convert_text, date_by_count, editor
+from tools import clients
+from tools import convert_text
+from tools import date_by_count, editor
 
-from tools.convert_text import convert_to_string_format, input_file_path
+from tools.clients import client1, client2, client3, clients, show_client
+from tools.convert_text import convert_text, convert_to_string_format
 from tools.date_by_count import (
     bebra_wrapper,
     choose_date,
     count_users,
     exclude_users,
-    show_client,
     start,
 )
 from tools.editor import remove_punct
@@ -14,13 +16,16 @@ from tools.editor import remove_punct
 __all__ = [
     "bebra_wrapper",
     "choose_date",
+    "client1",
+    "client2",
+    "client3",
+    "clients",
     "convert_text",
     "convert_to_string_format",
     "count_users",
     "date_by_count",
     "editor",
     "exclude_users",
-    "input_file_path",
     "remove_punct",
     "show_client",
     "start",
