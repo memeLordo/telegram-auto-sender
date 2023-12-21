b1c30f08e917ca0623fe11962379ff5809eb0d38
memeLordo <serafim.kripachev@gmail.com>
2023-12-21 15:30:37 +0300

    golubin update

--- golubin/golubin.py
+++ golubin/golubin.py
@@ -1,79 +1,88 @@
-# from telethon.types import User
 import asyncio
 
-from config.messages import Lead
+# from config.messages import Lead
 from loguru import logger
-from telethon.types import User
+# from telethon.types import User
+from tools.checker import is_user
 from tools.editor import make_plain
 
 from .clients import client1, client2, client3, show_client
 
 
-async def start() -> None:
-    bebra_set = []
-    async for dialog in client1.iter_dialogs():
-        if dialog.name == "Golubin | Assistant":
-            golubin = dialog.entity
-            break
-    async for message in client1.iter_messages(entity=golubin):
+async def make_user_list():
+    golubin = await find_golubin()
+    user_list = []
+    async for message in client.iter_messages(entity=golubin):
         try:
             text_set = set(make_plain(message.message).split(" "))
-            bebra = None
-            for word in text_set:
-                if word and "@" in word[0]:
-                    nickname = word.strip("@")
-                    if nickname is None:
+            # logger.info(text_set)
+            usernames = {wrd[1:] for wrd in text_set if wrd and wrd[0] == "@"}
+            if usernames:
+                logger.info(usernames)
+                for username in usernames:
+                    try:
+                        user = await client.get_entity(username)
+                        if not is_user(user):
+                            continue
+                        history = await user.get_messages(user)
+                        await asyncio.sleep(2)
+                        if history:
+                            continue
+                        first_name = user.first_name.split(" ")[0]
+                        user_list.append((username, first_name))
+                    except Exception:
                         continue
-
-                    bebra = await client1.get_entity("@" + nickname)
-                if not bebra or not isinstance(bebra, User) or bebra.bot:
-                    continue
-                if bebra in bebra_set:
-                    continue
-                logger.info(bebra.username)
-                bebra_set.append(bebra)
-            if len(bebra_set) == 50:
-                global names_list
-                names_list = bebra_set
-                break
-        except Exception:
-            continue
-        except TypeError:
-            continue
-    # await send_messages_to_leads(golubin, messages)
+            if len(user_list) >= 50:
+                return user_list
+        except Exception as e:
+            logger.critical(repr(e))
 
 
-async def send_messages_to_leads(client, range_list, func):
-    await client.start()
-    for bebra in range_list:
-        first_name = bebra.first_name.split(" ")[0]
-        # await asyncio.sleep(1)
-        # async with client.action(bebra, "typing"):
-        await asyncio.sleep(4)
-        await client.send_message(bebra, func(first_name))
-        # print(func(first_name))
-        logger.debug(
-            f"{show_client(client)}: message sent to {bebra.username}")
-    await client.disconnect()
-
-
-def divide_names():
+def divide_names(names_list):
     k, _ = divmod(len(names_list), 2)
-    first, second = names_list[:k], names_list[k:]
-    return [first, second]
+    return names_list[:k], names_list[k:]
+
+
+async def find_golubin():
+    async for dialog in client.iter_dialogs():
+        if dialog.name == "Golubin | Assistant":
+            return dialog.entity
+
+
+# async def start():
+#     result = await make_user_list()
+#     return result
+# user = await client.get_entity("ram_tim")
+# await client.send_message(user, Lead.say_hi("Рамилия"))
+
+
+async def send_messages_in_list(range_list):
+    for username, name in range_list:
+        bebra = await client.get_entity(username)
+        print(bebra)
+        # await asyncio.sleep(60)
+        # await client.send_message(bebra, Lead.say_hi(name))
+        # print(func(first_name))
+        logger.debug(f"{show_client(client)}:message sent to {username}")
+    await client.disconnect()
 
 
 @logger.catch
 def main() -> None:
     try:
-        client1.start()
-        client1.session.save_entities = False
-        client1.loop.run_until_complete(start())
-        client1.disconnect()
-        names = divide_names()
-        # global client
-
-        asyncio.run(send_messages_to_leads(client2, names[0], Lead.say_hi))
-        asyncio.run(send_messages_to_leads(client3, names[1], Lead.say_hi1))
+        global client
+        with client1 as client:
+            client.session.save_entities = False
+            user_list = client.loop.run_until_complete(make_user_list())
+        first, second = divide_names(user_list)
+        with client2 as client:
+            client.loop.run_until_complete(send_messages_in_list(first))
+        with client3 as client:
+            client.loop.run_until_complete(send_messages_in_list(second))
+        logger.info("done")
     except KeyboardInterrupt:
         pass
+
+
+if __name__ == "__main__":
+    main()
