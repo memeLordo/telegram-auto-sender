71365c1e6b9a9b78cc3c8bf9cbc8fc3545005bb8
memeLordo <serafim.kripachev@gmail.com>
2024-01-21 15:11:51 +0300

    Refactor wording in config

--- config/from_text.py
+++ config/from_text.py
@@ -1,3 +1,3 @@
 def read_add_message() -> str:
-    with open("config/script.txt", "r+", encoding="utf-8") as file:
+    with open("advertising/scriptq.txt", "r+", encoding="utf-8") as file:
         return file.read()
